be5f89d0e0fe85c4aaa5b230cbc96dfa72ae85d3
younasbangash <younas.appcom@gmail.com>
2016-03-14 16:49:07 +0400

    Initial Commit

--- app/src/main/java/com/pratap/endlessrecyclerview/MainActivity.java
+++ app/src/main/java/com/pratap/endlessrecyclerview/MainActivity.java
@@ -6,11 +6,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     private LinearLayoutManager mLayoutManager;
     private int mStart=0,mEnd=20;
     private List<Student> studentList;
+    public static int pageNumber;
 
 
     protected Handler handler;
@@ -30,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pageNumber = 1;
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         tvEmptyView = (TextView) findViewById(R.id.empty_view);
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
@@ -41,73 +54,99 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-        loadData(mStart,mEnd,false);
-
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
         mRecyclerView.setHasFixedSize(true);
-
         mLayoutManager = new LinearLayoutManager(this);
-
-        // use a linear layout manager
         mRecyclerView.setLayoutManager(mLayoutManager);
-
-        // create an Object for Adapter
         mAdapter = new DataAdapter(studentList, mRecyclerView);
-
-        // set the adapter object to the Recyclerview
         mRecyclerView.setAdapter(mAdapter);
-        //	 mAdapter.notifyDataSetChanged();
 
-
-        if (studentList.isEmpty()) {
-            mRecyclerView.setVisibility(View.GONE);
-            tvEmptyView.setVisibility(View.VISIBLE);
-
-        } else {
-            mRecyclerView.setVisibility(View.VISIBLE);
-            tvEmptyView.setVisibility(View.GONE);
-        }
+        GetGroupData(""+mStart,""+mEnd);
 
         mAdapter.setOnLoadMoreListener(new OnLoadMoreListener() {
             @Override
             public void onLoadMore() {
-                //add null , so the adapter will check view_type and show progress bar at bottom
                 studentList.add(null);
                 mAdapter.notifyItemInserted(studentList.size() - 1);
-
-
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        //   remove progress item
-                        studentList.remove(studentList.size() - 1);
-                        mAdapter.notifyItemRemoved(studentList.size());
-                        //add items one by one
-                        int start = studentList.size();
-                        int end = start + 20;
-                        loadData(start,end,true);
-                        /*for (int i = start + 1; i <= end; i++) {
-                            studentList.add(new Student("Student " + i, "AndroidStudent" + i + "@gmail.com"));
-                            mAdapter.notifyItemInserted(studentList.size());
-                        }*/
-                        mAdapter.setLoaded();
-
-                    }
-                }, 2000);
+                int start = studentList.size();
+                int end = start + 20;
+                ++pageNumber;
+                GetGroupData(""+start,""+end);
             }
         });
     }
 
+    public void GetGroupData(String LimitStart, String LimitEnd) {
+        Map<String, String> params = new HashMap<>();
+        params.put("LimitStart", LimitStart);
+        params.put("LimitEnd", LimitEnd);
+        Custom_Volly_Request jsonObjReq = new Custom_Volly_Request(Request.Method.POST,
+                "autoload.php", params,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("ResponseSuccess",response.toString());
+                        int json_status ;
+                        try {
+                            JSONArray catalouge;
+                            json_status = response.getInt("success");
+                            if (json_status == 1) {
+                                if (pageNumber > 1) {
+                                    studentList.remove(studentList.size() - 1);
+                                    mAdapter.notifyItemRemoved(studentList.size());
+                                }
+                                catalouge = response.getJSONArray("orders");
+                                for (int i = 0; i < catalouge.length(); i++) {
+                                    JSONObject jobj = catalouge.getJSONObject(i);
+                                    Student st;
+                                    st= new Student(jobj.getString("bname"),"");
+                                    studentList.add(st);
+                                    handler.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            mAdapter.notifyItemInserted(studentList.size());
+                                        }
+                                    });
+                                }
+                                mAdapter.setLoaded();
+                            }
 
+//                            mAdapter = new BuyPropertyAdapter(mResult,getApplicationContext());
+//                            recyclerView.setAdapter(mAdapter);
+//                            pDialog.setVisibility(View.GONE);
+//                            mAdapter.setOnItemClickListener(new BuyPropertyAdapter.MyClickListener() {
+//                                @Override
+//                                public void onItemClick(int position, View v) {
+//                                    if(mMixCatalougeCheck){
+//                                        Intent a1 = new Intent(CustomizeCatalougeMainActivity.this, MixCatalougeMainActivity.class);
+//                                        a1.putExtra("HeadCategory",mResult.get(position).getmHeadCategory());
+//                                        a1.putExtra("CollectionID",mResult.get(position).getMcollection_id());
+//                                        a1.putExtra("BrandName", mBrandName);
+//                                        a1.putExtra("BrandID", mBrandID);
+//                                        startActivity(a1);
+//                                    }
+//                                }
+//                            });
+                        }
+                        catch (JSONException e) {
+                            Log.d("ResponseError",e.toString());
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("ResponseErrorVolly: " + error.getMessage());
+
+            }});
+        AppController.getInstance().addToRequestQueue(jsonObjReq);
+    }
     // load initial data
     private void loadData(int start,int end,boolean notifyadapter) {
-
         for (int i = start; i <= end; i++) {
             studentList.add(new Student("Student " + i, "androidstudent" + i + "@gmail.com"));
             if(notifyadapter)
                 mAdapter.notifyItemInserted(studentList.size());
-
         }
     }
 }
